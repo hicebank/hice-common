b1f7b263375d0ce4e1c58d58de10b67469df3c83
Тимур Зайнуллин <tvzaynullin@gmail.com>
2021-09-16 14:43:50 +0300

    HB-2156 Add py.typed for mypy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='hice_common',
-    version='0.0.1',
+    version='0.0.2',
     author='hicebank.ru',
     author_email='tingaev@hicebank.ru',
     description='Phone number validator via pydantic',
@@ -19,4 +19,7 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+    package_data={
+        "hice_common": ["py.typed"],
+    },
 )
