ff75bce180a171a31f59157180f1f93cbb9e6970
Malik <minubaev@hicebank.ru>
2022-01-12 12:09:33 +0300

    fix/phone_validation

--- hice_common/validators.py
+++ hice_common/validators.py
@@ -9,15 +9,17 @@ class ValidationError(ValueError):
 
 
 def validate_phone_number(phone_number: str) -> str:
-
     if not isinstance(phone_number, str):
         raise ValidationError('phone_number should be passed as string')
 
     digits = ''.join(c for c in phone_number if c.isdigit())
-    if len(digits) < 9 or len(digits) > 12:
-        raise ValidationError('wrong size of phone number, it can be bettwen 10 and 11')
-    digits = f'7{digits[-10:]}'
+    if not 9 < len(digits) < 12:
+        raise ValidationError('wrong size of phone number, it can be between 10 and 11')
+
+    if (digits.startswith('9') and len(digits) == 10) or (digits.startswith('8') and len(digits) == 11):
+        digits = f'7{digits[-10:]}'
 
     if re.match(r'^79\d{9}$', digits) is None:
-        raise ValidationError('wrong value of phone number, it can be bettwen 10 and 11')
+        raise ValidationError('phone number must start from 79')
+
     return digits
